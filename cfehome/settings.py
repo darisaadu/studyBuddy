9f2402a2803772b98f542d9df81c5f8f1db7c158
tanimu <tanimusaadu2@gmail.com>
2023-12-21 15:16:38 +1300

    updated settings file

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u$@t!#v(%dmv8&34@fv=!4cb6!s(ly5lprln$=6r04jki0nu_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['./vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition

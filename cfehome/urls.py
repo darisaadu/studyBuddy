953459f5a084a12098a12bf262214d0db770f881
Tanimu Saadu <tanimusaadu2@gmail.com>
2023-12-23 01:07:05 +1300

    updated

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from cfehome.views import test
 
 
 urlpatterns = [
+    path('', test),
     path('admin/', admin.site.urls),
     path('base/', include('base.urls'))
 ]
